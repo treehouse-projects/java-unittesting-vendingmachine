55bea3ce4a699f6dcfa9c2c86dcf03341f0ceaff
Craig Dennis <craig@teamtreehouse.com>
2015-11-03 15:01:56 -0800

    Adds better named test

--- tests/com/teamtreehouse/vending/CreditorTest.java
+++ tests/com/teamtreehouse/vending/CreditorTest.java
@@ -4,13 +4,16 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by weasley on 11/3/15.
- */
 public class CreditorTest {
 
     @Test
     public void testRefund() throws Exception {
+        fail();
+    }
+
+    @Test
+    public void addingFundsIncrementsAvailableFunds() throws Exception {
+
 
     }
 }
