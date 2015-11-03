66acab7773c988b587c7085c260a3fc857becc67
Craig Dennis <craig@teamtreehouse.com>
2015-11-03 15:26:50 -0800

    Adds second test with a no no multi assert

--- tests/com/teamtreehouse/vending/CreditorTest.java
+++ tests/com/teamtreehouse/vending/CreditorTest.java
@@ -7,13 +7,24 @@ import static org.junit.Assert.*;
 public class CreditorTest {
 
     @Test
-    public void testRefund() throws Exception {
-        fail();
+    public void addingFundsIncrementsAvailableFunds() throws Exception {
+        Creditor creditor = new Creditor();
+
+        creditor.addFunds(25);
+        creditor.addFunds(25);
+
+        assertEquals(50, creditor.getAvailableFunds());
     }
 
     @Test
-    public void addingFundsIncrementsAvailableFunds() throws Exception {
+    public void refundingReturnsAllAvailableFunds() throws Exception {
+        Creditor creditor = new Creditor();
+        creditor.addFunds(10);
 
+        int refund = creditor.refund();
+
+        assertEquals(10, refund);
+        assertEquals(0, creditor.getAvailableFunds());
 
     }
 }
