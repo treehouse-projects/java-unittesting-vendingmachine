08fd0bcba239562d3bad4843f8c16bd38d198004
Craig Dennis <craig@teamtreehouse.com>
2015-11-03 14:43:17 -0800

    Adds tests sources root and first bunk test

---
+++ tests/com/teamtreehouse/vending/CreditorTest.java
@@ -0,0 +1,16 @@
+package com.teamtreehouse.vending;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by weasley on 11/3/15.
+ */
+public class CreditorTest {
+
+    @Test
+    public void testRefund() throws Exception {
+
+    }
+}
